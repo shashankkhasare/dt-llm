61ddacf7c6d9b39ed264ddd9493a7180ccc3d3b8
Shashank Khasare <shashank.khasare@gmail.com>
2023-07-06 08:09:29 +0530

    WIP: Add code to get tree JSON.

--- classifier.py
+++ classifier.py
@@ -21,7 +21,7 @@ keybert_model = KeyBERT(model)
 selected_features = set()
 for i, text in enumerate(texts):
     print(f"review number: {i}")
-    keywords = keybert_model.extract_keywords(text, keyphrase_ngram_range=(1, 2), stop_words='english')
+    keywords = keybert_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words='english')
     selected_features.update([keyword[0] for keyword in keywords])
 
 # Convert the selected features to a feature matrix
@@ -38,6 +38,28 @@ tree.fit(feature_matrix, labels)
 # Print the decision tree
 tree_text = export_text(tree, feature_names=list(selected_features))
 print(tree_text)
+
+# Convert the decision tree to JSON-like structure
+def tree_to_dict(tree, feature_names):
+    tree_dict = {}
+    if tree.children_left[0] == -1:  # Leaf node
+        tree_dict['class'] = int(tree.value[0, 0])
+    else:  # Internal node
+        feature_index = tree.feature[0]
+        threshold = tree.threshold[0]
+        feature_name = feature_names[feature_index]
+        tree_dict['feature'] = feature_name
+        tree_dict['threshold'] = float(threshold)
+        tree_dict['left'] = tree_to_dict(tree.tree_.children_left[0], feature_names)
+        tree_dict['right'] = tree_to_dict(tree.tree_.children_right[0], feature_names)
+    return tree_dict
+
+tree_dict = tree_to_dict(tree.tree_, selected_features)
+tree_json = json.dumps(tree_dict, indent=4)
+text_file = open("tree.json", "w")
+n = text_file.write(tree_json)
+text_file.close()
+print(tree_json)
 
 # Use the decision tree for prediction
 new_texts = dataset["test"]["text"]
